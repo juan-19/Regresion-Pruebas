11824df7a623dc8c3fb48173e9f0c0c6d8927b5e
Juan <root@juan-Inspiron-5437>
2018-09-27 14:26:11 -0600

    agregando cambios

--- jenkins.sh
+++ jenkins.sh
@@ -6,7 +6,7 @@ line=1
 if [ $line -eq $var ]; then
     	sudo git add .
         sudo git commit -m "agregando cambios"
-	sudo git push https://github.com/juan-19:narusake100/Regresion-Pruebas.git
+	sudo git push https://github.com/juan-19/Regresion-Pruebas.git
         echo "git repo updated"
 else
         echo "Fallo las pruebas"
