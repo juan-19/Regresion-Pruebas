70616877f65f64a36a520defbde666742eca630b
Juan <root@juan-Inspiron-5437>
2018-09-27 13:53:46 -0600

    agregando cambios

---
+++ alu_env.sv
@@ -0,0 +1,7 @@
+alu_env.sv		  leer		prueba.txt	  test.cpp
+backup.txt		  nombre	regresion	  test.sv
+bash_snps_xt018-AMS_2017  nombre.cpp	salida.txt	  test.txt
+design.sv		  nombre.txt	script		  upload.sh
+file			  numeros.cpp	sim_cov_commands
+file.txt		  pruebas2.cpp	test
+git.sh			  pruebas.cpp	testbench.sv
