2a52ae366656028a99bc9422fef8ba27ad4aac69
Juan <root@juan-Inspiron-5437>
2018-09-27 14:02:54 -0600

    algo

--- alu_env.sv
+++
@@ -1,7 +0,0 @@
-alu_env.sv		  leer		prueba.txt	  test.cpp
-backup.txt		  nombre	regresion	  test.sv
-bash_snps_xt018-AMS_2017  nombre.cpp	salida.txt	  test.txt
-design.sv		  nombre.txt	script		  upload.sh
-file			  numeros.cpp	sim_cov_commands
-file.txt		  pruebas2.cpp	test
-git.sh			  pruebas.cpp	testbench.sv
