defab1f6afce3cb979ac559acb1b0aa646f9f32f
Jenkins <jenkins@juan-Inspiron-5437>
2018-10-03 09:14:12 -0600

    agregando cambios

---
+++ alu_interface.sv
@@ -0,0 +1,8 @@
+alu_agent.sv		  file.txt     pruebas2.cpp	 test
+alu_driver.sv		  git.sh       pruebas.cpp	 testbench.sv
+alu_env.sv		  jenkins.sh   prueba.txt	 test.cpp
+alu_interface.sv	  leer	       README.md	 test.sv
+backup.txt		  nombre       regresion	 test.txt
+bash_snps_xt018-AMS_2017  nombre.cpp   salida.txt	 upload.sh
+design.sv		  nombre.txt   script
+file			  numeros.cpp  sim_cov_commands
