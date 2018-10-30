d3290a721a0b7118bfc09e6691e8ba9b3dd5945b
Jenkins <jenkins@juan-Inspiron-5437>
2018-10-30 12:53:48 -0600

    agregando cambios

--- design.sv
+++ design.sv
@@ -3,3 +3,4 @@ bash_snps_xt018-AMS_2017  leer	      pruebas2.cpp  script	      test.sv
 design.sv		  nombre      pruebas.cpp   sim_cov_commands  test.txt
 file			  nombre.cpp  prueba.txt    test	      upload.sh
 file.txt		  nombre.txt  regresion     testbench.sv
+// Code your design here
